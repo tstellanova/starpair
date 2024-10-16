a4b324136c100c3afa6b67aa9a8c19563803b122
Todd Stellanova <tstellanova@users.noreply.github.com>
2024-10-16 15:40:20 +0200

    filtering unique origins

--- starpair/download_by_neighbor.py
+++ starpair/download_by_neighbor.py
@@ -11,21 +11,11 @@ import csv
 def query_nearby_stars_batch(center_source_id, coordinates, max_dist_pc: float = 100.):
     all_results = []
     for l, b in coordinates:
-        # query = f"""
-        # SELECT gaia.SOURCE_ID,gaia.l,gaia.b,ABS(1000./gaia.parallax) AS dist_pc,
-        #     gaia.distance_gspphot,gaia.parallax,gaia.ra,gaia.dec,gaia.phot_g_mean_mag
-        # FROM gaiadr3.gaia_source AS gaia
-        # WHERE DISTANCE(POINT('GALACTIC', gaia.l, gaia.b), POINT('GALACTIC', {l}, {b})) < 0.25
-        # AND gaia.parallax is not NULL
-        # AND gaia.parallax > 0
-        # AND 1000.0/gaia.parallax <= {max_dist_pc}
-        # """
-
         query = f"""
         SELECT SOURCE_ID,l,b,  
             distance_gspphot,parallax,ra,dec,phot_g_mean_mag,
             DISTANCE( POINT({l},{b}), POINT(l, b)) AS ang_sep,
-            1000./parallax AS dist_pc
+            (1000.0/parallax) AS dist_pc
         FROM gaiadr3.gaia_source
         WHERE 1 = CONTAINS( POINT({l},{b}), CIRCLE(l, b, 0.25))
         AND SOURCE_ID != {center_source_id}
@@ -41,6 +31,7 @@ def query_nearby_stars_batch(center_source_id, coordinates, max_dist_pc: float =
     return np.vstack(all_results) if all_results else None
 
 g_habstar_by_id_map = {}
+g_origstar_by_id_map = {}
 
 def main():
     parser = argparse.ArgumentParser(description='Download stars near other stars of interest')
@@ -60,10 +51,11 @@ def main():
     outfile_prefix = f"{out_dir}neighbors_L{total_gaia_objects:0.0e}"
     max_dist_pc = 100
     max_ang_sep = 0.25
-    int_ang_sep = 1000 * max_ang_sep
-    filtered_outfile_name = f"{outfile_prefix}_a{int_ang_sep}_d{max_dist_pc}.csv"
+    int_ang_sep = int(1000 * max_ang_sep)
+    duplicate_output_filename = f"{outfile_prefix}_a{int_ang_sep}_d{max_dist_pc}.csv"
+    unique_origins_output_filename = f"{out_dir}uniquepairs_L{total_gaia_objects:0.0e}_a{int_ang_sep}_d{max_dist_pc}.csv"
 
-    print(f"Loading TOIs from: {toi_path} , output to: {filtered_outfile_name}")
+    print(f"Loading TOIs from: {toi_path} , output to: {duplicate_output_filename}")
 
     hab_list_row_count = 0
     # import the habitable star system list file
@@ -83,11 +75,12 @@ def main():
 
     # setup the output file
     field_names = ["orig_id","dest_id","orig_dist","dest_dist","ang_sep","radial_sep","orig_coord","dest_coord"]
-    file_ref = open(filtered_outfile_name, 'a')
+    file_ref = open(duplicate_output_filename, 'w')
     csv_writer = csv.writer(file_ref)
     csv_writer.writerow(field_names)
     file_ref.flush()
 
+    perf_start_all_neighbors = perf_counter()
     evaluated_hstars_count = 0
     total_star_pairs = 0
     for hab_star_source_id in g_habstar_by_id_map.keys():
@@ -105,6 +98,7 @@ def main():
         # Query for all neighbor stars for a single habstar
         neighbors = query_nearby_stars_batch(hab_star_source_id, habstar_raw_coord)
         evaluated_hstars_count += 1
+        duplicate_origins_count = 0
         n_new_neighbors = 0
         if neighbors is not None:
             neighbors = neighbors[0]
@@ -117,6 +111,10 @@ def main():
                     neighbor_radial_dist_pc = np.float64(neighbor_gspphot_pc)
                 else:
                     neighbor_radial_dist_pc = np.float64(neighbor['dist_pc'])
+                if np.isnan(neighbor_radial_dist_pc):
+                    neighbor_parallax = np.float64(neighbor['parallax'])
+                    print(f"reprocess parallax {neighbor_parallax} from {neighbor['dist_pc']}")
+                    neighbor_radial_dist_pc = 1000.0/neighbor_parallax
 
                 neighbor_id = np.uint64(neighbor['SOURCE_ID'])
                 neighbor_l = np.float64(neighbor['l'])
@@ -124,6 +122,8 @@ def main():
                 neighbor_coord_str = f"{neighbor_l:0.4f} {neighbor_b:0.4f} {neighbor_radial_dist_pc:0.4f}"
 
                 radial_sep = neighbor_radial_dist_pc - habstar_radial_dist_pc
+                if np.isnan(radial_sep):
+                    print(f"{neighbor_id} bad sep: {neighbor_radial_dist_pc} - {habstar_radial_dist_pc}")
                 if radial_sep > 0:
                     origin_id = neighbor_id
                     dest_id = hab_star_source_id
@@ -138,20 +138,40 @@ def main():
                     dest_dist = neighbor_radial_dist_pc
                     origin_coord = habstar_coord_str
                     dest_coord = neighbor_coord_str
-
-                out_row = (origin_id, dest_id, origin_dist, dest_dist, ang_sep, np.abs(radial_sep), origin_coord, dest_coord)
+                radial_sep = np.abs(radial_sep)
+                out_row = (origin_id, dest_id, origin_dist, dest_dist, ang_sep, radial_sep, origin_coord, dest_coord)
                 csv_writer.writerow(out_row)
+                existing_orig = g_origstar_by_id_map.get(origin_id)
+                if existing_orig is None:
+                    g_origstar_by_id_map[origin_id] = out_row
+                else:
+                    duplicate_origins_count += 1
+                    existing_radial_sep = existing_orig[5]
+                    if radial_sep < existing_radial_sep:
+                        g_origstar_by_id_map[origin_id] = out_row
+                    print(f"{duplicate_origins_count} dup orig: {origin_id} -> dest: {dest_id} rsep new: {radial_sep} old: {existing_radial_sep}")
+
             file_ref.flush()
 
         print(f"{evaluated_hstars_count}/{n_concrete_habitable_stars} {hab_star_source_id} has: {n_new_neighbors} neighbors")
 
-
     file_ref.flush()
     file_ref.close()
-    print(f"Total pairs: {total_star_pairs}")
-    print(f"Wrote to: \n{filtered_outfile_name} ")
+    file_ref = None
+    print(f"Total pairs: {total_star_pairs} duplicates: {duplicate_origins_count}")
+    print(f"Total unique origins: {len(g_origstar_by_id_map)}")
+    print(f"Neighbor searching >> elapsed: {perf_counter() - perf_start_all_neighbors:0.3f} seconds")
+    print(f"Wrote to: \n{duplicate_output_filename} ")
 
-
+    # Now write condensed info with unique origin IDs to a separate file
+    # field_names = ["orig_id","dest_id","orig_dist","dest_dist","ang_sep","radial_sep","orig_coord","dest_coord"]
+    file_ref = open(unique_origins_output_filename, 'w')
+    csv_writer = csv.writer(file_ref)
+    csv_writer.writerow(field_names)
+    file_ref.flush()
+    csv_writer.writerows(g_origstar_by_id_map.values())
+    file_ref.flush()
+    file_ref.close()
 
 if __name__ == "__main__":
     main()
